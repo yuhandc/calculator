993d65ea14ee0c4d208f12b3b02eaa1a72d1bba2
Yuhan Chandrawansa <yuhan.chandrawansa@assurity.co.nz>
2020-02-07 12:47:59 +1300

    initial commit

---
+++ src/test/java/calculator/CalculatorSteps.java
@@ -0,0 +1,20 @@
+package calculator;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorSteps {
+    private int result;
+
+    @When("I calculate {int} + {int}")
+    public void iCalculate(int arg0, int arg1) {
+        result = arg0 + arg1;
+    }
+
+    @Then("the answer is {int}")
+    public void theAnswerIs(int arg0) {
+        assertEquals(arg0, result);
+    }
+}
---
+++ src/test/resources/calculator/basic_sums.feature
@@ -0,0 +1,12 @@
+Feature: Basic Sums
+  As a business customer
+  I want to automate my basic sums
+  So that I can get correct answers quickly
+
+  Acceptance Criteria
+  -------------------
+  1. I can add, subtract, multiply or divide any two numbers.
+
+  Scenario: Basic Addition
+    When I calculate 2 + 2
+    Then the answer is 4
